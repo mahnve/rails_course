4fbc73b36c564aa496a55fc80014445a74fb9b15
Marcus Ahnve <marcus@ahnve.com>
2011-12-27 10:43:49 +0100

    initial rails app

---
+++ blogg/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blogg::Application.config.secret_token = 'f66825b2c5ee59ff1897e06ca24fe6081d7dc0bb19ac90b60a568618fef478bdf2f3e195dfaf1321f8d18ac8e03a40fb6e1ea69a20030590e3638b1bf07ff7b5'
---
+++ installing/01_slides.md
@@ -0,0 +1,21 @@
+<! SLIDE commandline >
+
+$ rvm 1.9.3 
+
+<! SLIDE commandline >
+
+$ gem install rails
+
+...
+
+<! SLIDE commandline >
+
+$ rails new blogg -d postgresql
+
+... 
+
+Your bundle is complete! Use `bundle show [gemname]` to see where a bundled
+gem is installed
+
+
+
